8be54f28449393a5238199ddeea99e7627cbeb0b
edp999 <edipietro23@gmail.com>
2022-11-24 14:14:22 +0100

    14.14

--- LAMP-java/TestLampadina.java
+++ LAMP-java/TestLampadina.java
@@ -1,15 +1,30 @@
-
+import java.util.Scanner;
 
 public class TestLampadina {
     public static void test(int maxClick){
         Lampadina l  = new Lampadina(maxClick);
         for(int i=0; i<=maxClick; i++){
-            l.click();
-            l.stato();
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Click? Y/N:  ");
+            String response = scan.next();
+            if (response.equals("Y")){
+                l.click();
+                l.stato();
+            }
+            else if (response.equals("N")){
+                l.stato();
+                i-=1;
+            }
+            Scanner scan2 = new Scanner(System.in);
+            System.out.print("Continue? Y/N:  ");
+            String response2 = scan2.next();
+            if (response2.equals("N")){
+                break;
+            }
         }
     }
-
     public static void main(String[] args) {
         test(5);
     }
+    
 }
